a71ff917123a40b8ac6bdbb4e4405853e5e9d123
victorpessoa01 <auserofficial007@gmail.com>
2022-11-18 11:10:58 -0300

    troca do designer

--- DeskShop/TelaMenu.cs
+++ DeskShop/TelaMenu.cs
@@ -20,39 +20,13 @@ namespace DeskShop
             acesso.Close();
         }               
         
-        private void pictureBoxMax_Click(object sender, EventArgs e)
-        {
-            this.WindowState = FormWindowState.Maximized;
-        }
-
-        private void pictureBoxNormal_Click(object sender, EventArgs e)
-        {
-            this.WindowState = FormWindowState.Normal;
-        }
-
-        private void pictureBoxMin_Click(object sender, EventArgs e)
-        {
-            this.WindowState = FormWindowState.Minimized;
-        }
-
         private void bntLogout_Click(object sender, EventArgs e)
         {
             if (MessageBox.Show("Deseja Fechar o Programa?","DeskShop",MessageBoxButtons.YesNo,MessageBoxIcon.Question) == DialogResult.Yes)
             { 
-                Acesso acesso = new Acesso();
-                acesso.Close();
                 Close();
             }
-
-            else
-            {
-                Close();
-
-                Acesso acesso = new Acesso();
-                acesso.Show();
-                this.Dispose(); 
             }
-        }
 
         private void panelCentro_Paint(object sender, PaintEventArgs e)
         {
